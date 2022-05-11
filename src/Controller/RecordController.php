573eea6a39cc7a6f66cbd7a134f85ebd26626714
Kasia <you@example.com>
2022-05-11 07:33:39 +0000

    Twig test

---
+++ src/Controller/RecordController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\RecordRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[ Route('/record') ]
+class RecordController extends AbstractController
+{
+    #[Route(
+        '',
+        name: 'record_index',
+        methods: 'GET'
+    )]
+    public function index(RecordRepository $repository): Response
+    {
+        $records = $repository->findAll();
+        return $this->render(
+            'record/index.html.twig',
+            ['records' => $records]
+
+        );
+    }
+}
